1bb0227b2c2332d9f8135aa24ab30fd250089dbc
Thomas Madden <tmadden@gmail.com>
2017-07-19 00:31:02 +0000

    Add missing include.

--- src/cradle/core/type_interfaces.cpp
+++ src/cradle/core/type_interfaces.cpp
@@ -2,6 +2,7 @@
 
 #include <boost/date_time/gregorian/gregorian.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/format.hpp>
 #include <boost/numeric/conversion/cast.hpp>
 
 #include <cradle/core/value.hpp>
