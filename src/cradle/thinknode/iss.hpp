e195e69971d2459315f289c2f524e28beec7a214
Thomas Madden <tmadden@mgh.harvard.edu>
2017-07-25 14:40:54 -0400

    Add ISS POST utilities.

--- src/cradle/thinknode/iss.hpp
+++ src/cradle/thinknode/iss.hpp
@@ -23,6 +23,19 @@ retrieve_immutable(
     string const& context_id,
     string const& immutable_id);
 
+// Get the URL form of a schema.
+string
+get_url_type_string(api_type_info const& schema);
+
+// Post an ISS object and return its ID.
+string
+post_iss_object(
+    http_connection_interface& connection,
+    thinknode_session const& session,
+    string const& context_id,
+    api_type_info const& schema,
+    value const& data);
+
 }
 
 #endif
