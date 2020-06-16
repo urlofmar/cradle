fddddf772e79c541a38552b9e223365905f3948d
Thomas Madden <tmadden@gmail.com>
2020-06-16 13:08:35 -0400

    Explicitly install make.

--- scripts/set-up-system.sh
+++ scripts/set-up-system.sh
@@ -8,7 +8,7 @@ apt-get update -qy
 apt-get install -y software-properties-common apt-transport-https
 add-apt-repository -y ppa:ubuntu-toolchain-r/test
 apt-get update -qy
-apt-get install -y --upgrade python3 python3-dev g++-5 gcc-5 lcov git curl
+apt-get install -y --upgrade python3 python3-dev g++-5 gcc-5 lcov git curl make
 curl -sSL https://cmake.org/files/v3.17/cmake-3.17.2-Linux-x86_64.sh -o install-cmake.sh
 chmod +x install-cmake.sh
 ./install-cmake.sh --prefix=/usr/local --skip-license
