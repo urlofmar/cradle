97f8f546cf8ef9391c5662cb838e6a6f86fa56f2
Thomas Madden <tmadden@gmail.com>
2021-03-26 13:40:31 -0400

    Update Docker build

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM ubuntu:xenial as builder
+FROM ubuntu:focal as builder
 COPY . /cradle
 WORKDIR /cradle
 RUN scripts/docker-build.sh
 
-FROM ubuntu:xenial
+FROM ubuntu:focal
 COPY --from=builder /cradle/build/deploy /cradle
 COPY ./docker-config.json /root/.config/cradle/config.json
 WORKDIR /cradle
--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -x -e
-scripts/set-up-system.sh
-scripts/set-up-python.sh --python=python3.5
+apt install lcov ocaml-nox
+pip3 install virtualenv
+gcc --version
+scripts/set-up-python.sh --python=python3
 source .venv/bin/activate
-export CC=`which gcc-5`
-export CXX=`which g++-5`
-update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5
 scripts/set-up-conan.sh
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j server
