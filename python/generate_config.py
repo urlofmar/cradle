d5f2fed09ae48b0f0d078f2ff4b449e5b8c791ac
Thomas Madden <tmadden@gmail.com>
2021-08-24 12:30:44 -0400

    Fix another pyyaml warning

--- python/generate_config.py
+++ python/generate_config.py
@@ -2,9 +2,10 @@ import sys
 import os
 import yaml
 
-config_template_path = os.path.join(os.path.dirname(__file__), 'config_template.yml')
+config_template_path = os.path.join(os.path.dirname(__file__),
+                                    'config_template.yml')
 with open(config_template_path, 'r') as config_template_file:
-    config = yaml.load(config_template_file)
+    config = yaml.full_load(config_template_file)
 
 config['api_token'] = sys.argv[1]
 config['cradle_url'] = 'ws://localhost:41071'
