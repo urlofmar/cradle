ed43879e5ac685dd503e560c37b2d2a7f8d1b198
Thomas Madden <tmadden@gmail.com>
2021-10-04 14:50:38 -0400

    Update CI compiler versions

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -x -e
 source /scripts/.venv/bin/activate
+export CC=`which gcc-10`
+export CXX=`which gxx-10`
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
--- scripts/docker-setup.sh
+++ scripts/docker-setup.sh
@@ -2,7 +2,7 @@
 set -x -e
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install -y lcov ocaml-nox python3-pip cmake git
+apt-get install -y lcov ocaml-nox python3-pip cmake git gcc-10
 pip3 install virtualenv
-gcc --version
+gcc-10 --version
 ./set-up-python.sh --python=python3
