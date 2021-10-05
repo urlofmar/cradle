65907eed071f706b1bc4409433d4e4e8cf09fc8b
Thomas Madden <tmadden@gmail.com>
2021-10-05 14:11:18 -0400

    Fix Docker g++ selection

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -2,7 +2,7 @@
 set -x -e
 source /scripts/.venv/bin/activate
 export CC=`which gcc-10`
-export CXX=`which gxx-10`
+export CXX=`which g++-10`
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
