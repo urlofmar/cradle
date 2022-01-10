d21e762150e78013e58325283c023c02bd574791
Thomas Madden <tmadden@gmail.com>
2022-01-10 09:35:54 -0500

    Remove jinja dependency

--- scripts/set-up-python.sh
+++ scripts/set-up-python.sh
@@ -5,4 +5,4 @@ set -x -e
 virtualenv "$@" --prompt="(cradle) " .venv
 source .venv/bin/activate
 python --version
-pip install conan jinja2 gcovr pytest websocket-client msgpack
+pip install conan gcovr pytest websocket-client msgpack
